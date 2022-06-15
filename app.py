89c62b814ea43c136e944d202fdc125778482c60
rohanbagde001 <rohanbagde001@gmail.com>
2022-06-15 07:17:46 +0530

    updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():   # index page
-    return "Starting Machine Learning Project"
+    return "CI/CD pipeline has been established successfully,cheers"
 
 if __name__ == '__main__':
     app.run(debug=True) # run flask app in debug mode
